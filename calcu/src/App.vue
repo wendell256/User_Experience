b2304766dffa202bbe20413c00f1744c08543c55
wendell256 <wendell256@gmail.com>
2020-02-21 20:01:47 -0600

    añadiendo la implementación de la calcu

---
+++ calcu/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Calculator />
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Calculator from './components/HelloWorld.vue'
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+</style>
